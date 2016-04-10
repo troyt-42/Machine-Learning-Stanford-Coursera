533752d9d6d97e949023c0fcd280662df92e8a78
Tao_Mac <rnm2888@gmail.com>
2016-04-09 21:29:26 -0400

    Finish ex6

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -2,12 +2,15 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   (C, sigma) = EX6PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
 % You need to return the following variables correctly.
+candidate_c = [0.01,0.03,0.1,0.3,1,3,10,30];
+candidate_sigma = [0.01,0.03,0.1,0.3,1,3,10,30];
+errors = zeros(length(candidate_c));
 C = 1;
 sigma = 0.3;
 
@@ -15,15 +18,27 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
+for i = 1:length(candidate_c)
+  for p = 1:length(candidate_sigma)
+    temp_c = candidate_c(i);
+    temp_sigma = candidate_sigma(p);
+    model= svmTrain(X, y, temp_c, @(x1, x2) gaussianKernel(x1, x2, temp_sigma));
+    predictions = svmPredict(model, Xval);
+    errors(i, p) = mean(double(predictions ~= yval));
+  endfor
+endfor
 
-
+[v, index] = min(errors);
+[v, index2] = min(v);
+C = candidate_c(index(index2));
+sigma = candidate_sigma(index2);
 
 
 
